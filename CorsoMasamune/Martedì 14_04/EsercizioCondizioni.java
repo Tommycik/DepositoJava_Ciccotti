7cb8f4bafe933b47de83f065af1c3c7aca222a69
Tommycik <107619877+Tommycik@users.noreply.github.com>
2026-04-14 18:13:29 +0200

    Update EsercizioCondizioni.java
    correzione esercizio

--- CorsoMasamune/Martedì 14_04/EsercizioCondizioni.java
+++ CorsoMasamune/Martedì 14_04/EsercizioCondizioni.java
@@ -4,11 +4,11 @@ public class EsercizioCondizioni {
     public static void main(String[] args) {
         // Dichiarazione variabili
         int età;
-        boolean hasTicket;
-        boolean accompaniedByAdult;
-        boolean skipWaitingLine;
+        String hasTicket;
+        String accompaniedByAdult;
+        String skipWaitingLine;
         // Scanners per input
-        Scanner booleanScanner = new Scanner(System.in);
+        Scanner stringScanner = new Scanner(System.in);
         Scanner intScanner = new Scanner(System.in);
 
         // Input età
@@ -19,40 +19,40 @@ public class EsercizioCondizioni {
         System.out.println("La tua età è: " + età);
 
         // Input se ha il biglietto
-        System.out.println("Hai un biglietto? (true/false)");
+        System.out.println("Hai un biglietto? (si/no)");
         // Lettura input biglietto
-        hasTicket = booleanScanner.nextBoolean();
+        hasTicket = stringScanner.nextLine();
         // Output biglietto
         System.out.println("Hai un biglietto: " + hasTicket);
         
         // Input se è accompagnato da un adulto
-        System.out.println("Sei accompagnato da un adulto? (true/false)");
+        System.out.println("Sei accompagnato da un adulto? (si/no)");
         // Lettura input accompagnamento
-        accompaniedByAdult = booleanScanner.nextBoolean();
+        accompaniedByAdult = stringScanner.nextLine();
         // Output accompagnamento
         System.out.println("Sei accompagnato da un adulto: " + accompaniedByAdult);
         
         // Input se salta fila
-        System.out.println("Hai il salta fila? (true/false)");
+        System.out.println("Hai il salta fila? (si/no)");
         // Lettura input salto fila
-        skipWaitingLine = booleanScanner.nextBoolean();
+        skipWaitingLine = stringScanner.nextLine();
         // Output salta fila
         System.out.println("Hai il salta fila: " + skipWaitingLine);
         
         // Condizioni per l'ingresso
-        if(hasTicket){
+        if(hasTicket.equals("si")) {
             System.out.println("Puoi entrare!");
         } else {
             System.out.println("Non puoi entrare senza un biglietto.");
         }
 
-        if(accompaniedByAdult){
+        if(accompaniedByAdult.equals("si")){
             System.out.println("Sei accompaganto da un adulto, puoi entrare anche senza biglietto!");
         } else {
             System.out.println("Sei da solo.");
         }
 
-        if(skipWaitingLine){
+        if(skipWaitingLine.equals("sì")){
             System.out.println("Hai il salta fila, puoi entrare senza aspettare!");
         } else {
             System.out.println("Non hai il salta fila, dovrai aspettare in coda.");
