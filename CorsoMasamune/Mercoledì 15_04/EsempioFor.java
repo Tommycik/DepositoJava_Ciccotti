0ec9eb6e932a4ec1b7c3bc13f1106565a0707cb0
Tommycik <107619877+Tommycik@users.noreply.github.com>
2026-04-15 15:31:58 +0200

    esempio for

--- CorsoMasamune/Mercoledì 15_04/EsempioFor.java
+++ CorsoMasamune/Mercoledì 15_04/EsempioFor.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class EsempioFor {
     public static void main(String[] args) {
-        // Ciclo for per stampare i numeri da 1 a 5
-        for (int i = 1; i <= 5; i++) {
-            System.out.println(i);
+        Scanner scanner = new Scanner(System.in);
+        // Richiede numero
+        System.out.println("Inserisci un numero: ");
+        int numero = scanner.nextInt();
+        //Stampa la tabellina
+        System.out.println("Tabelina del " + numero + ":");
+        // Ciclo for per moltiplicare il numero da 1 a 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " * " + i + " = " + i*numero);
         }
+        scanner.close();
+
     }
 }
