cf36fb54e2b162e4922752b14c260ba7cbf7917b
Tommycik <107619877+Tommycik@users.noreply.github.com>
2026-04-15 15:21:50 +0200

    Mercoledì

---
+++ CorsoMasamune/Mercoledì 15_04/EsempioWhile.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class EsempioWhile {
+    public static void main(String[] args) {
+
+        int i = 1; // Inizializzazione della variabile di controllo
+        while (i <= 5) {
+        i++; // Incremento della variabile di controllo
+        System.out.println(i);
+        }
+        // Scanner per input
+        Scanner input = new Scanner(System.in);
+        int numero;
+        // Ciclo do-while per inserire numeri finché non viene inserito 0
+        do {
+            System.out.print("Inserisci un numero (0 per uscire): ");
+            numero = input.nextInt();
+        } while (numero != 0);
+        //Uscita dal ciclo quando viene inserito 0
+        System.out.println("Hai inserito 0. Programma terminato.");
+    }
+}
---
+++ CorsoMasamune/Mercoledì 15_04/EsercizioWhile.java
@@ -0,0 +1,46 @@
+public class EsercizioWhile {
+
+    public static void main(String[] args) {
+        // Dichiarazione variabili
+       String password;
+       String correctPassword = "java123";
+       int tentativi = 0;
+       boolean correctPasswordFound = false;
+        // Scanner per input   
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        // Ciclo per tentativi password
+        while(tentativi < 3) {
+            // Input password
+            System.out.println("Inserisci la password:");
+            password = scanner.nextLine();
+            // Incremento tentativi
+            tentativi++;
+            if(password.equals(correctPassword)) {
+                correctPasswordFound = true;
+                break;
+            }
+        }
+
+        String risposta;
+        if(correctPasswordFound) {
+            do{
+                //input risposta
+                System.out.println("Vuoi accedere al sistema? (s/n)");
+                risposta = scanner.nextLine();       
+            } while(!(risposta.equals("s") || risposta.equals("n")));
+            switch (risposta) {
+            case "s":
+                System.out.println("Accesso al sistema effettuato.");
+                break;
+            case "n":
+                System.out.println("Accesso al sistema annullato.");
+            default:
+                System.out.println("Risposta non valida.");
+                break;
+            }
+        }else {
+            System.out.println("Accesso bloccato.");
+        }
+        
+    }
+}
