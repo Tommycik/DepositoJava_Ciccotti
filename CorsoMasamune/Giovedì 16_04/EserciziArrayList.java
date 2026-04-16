77bb0f411cb02cc4f0d7c0922bfd2b6a101c2f6d
Tommycik <107619877+Tommycik@users.noreply.github.com>
2026-04-16 14:54:23 +0200

    arrayList

---
+++ CorsoMasamune/Giovedì 16_04/EserciziArrayList.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class EserciziArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> numeri = new ArrayList<>();
+        numeri.add(1);
+        numeri.add(2);
+        numeri.add(3);
+        System.out.println(numeri);
+    }
+}
