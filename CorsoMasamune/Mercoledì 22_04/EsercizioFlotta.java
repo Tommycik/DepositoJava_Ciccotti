1a6ded2295102fe0f28a4bfb13bf2567b33b030d
Tommycik <107619877+Tommycik@users.noreply.github.com>
2026-04-22 16:55:28 +0200

    Update EsercizioFlotta.java

--- CorsoMasamune/Mercoledì 22_04/EsercizioFlotta.java
+++ CorsoMasamune/Mercoledì 22_04/EsercizioFlotta.java
@@ -4,16 +4,22 @@ public class EsercizioFlotta {
     public static void main(String[] args) {
         //crea 2 aerei e 2 piloti
         Aereo aereo1 = new Aereo("A320", 4, "123456789");
-        Aereo aereo2 = new Aereo("A321", 4, "123456789");
+        //crea 2 aeri con lo stesso codice
+        Aereo aereo3 = new Aereo("A320", 4, "123456788");
+        Aereo aereo2 = new Aereo("A321", 4, "123456788");
         Pilota pilota1 = new Pilota("Tommy", 5, "123456789");
-        Pilota pilota = new Pilota("Maria", 5, "123456789");
+        //crea 2 piloti con lo stesso numero di brevetto
+        Pilota pilota2 = new Pilota("Maria", 5, "123456788");
+        Pilota pilota3 = new Pilota("Luca", 5, "123456788");
         //crea la compagnia
         CompagniaAerea compagnia = new CompagniaAerea("Compagnia 1");  
         //aggiungo i piloti e gli aerei
         compagnia.aggiungiPilota(pilota1);
-        compagnia.aggiungiPilota(pilota);   
+        compagnia.aggiungiPilota(pilota2);   
+        compagnia.aggiungiPilota(pilota3);
         compagnia.aggiungiAereo(aereo1);
         compagnia.aggiungiAereo(aereo2);
+        compagnia.aggiungiAereo(aereo3);
         //stampa informazioni della compagnia
         compagnia.stampa();
     }
@@ -125,10 +131,24 @@ class CompagniaAerea{
 
     //aggiungi aereo
     public void aggiungiAereo(Aereo a) {
+        //controlla non ci sia un altro aereo con lo stesso codice
+        for(Aereo a1 : flotta) {
+            if(a1.getCodice().equals(a.getCodice())) {
+                System.out.println("Aereo già presente nella lista");
+                return;
+            }
+        }
         flotta.add(a);
     }
     //aggiungi pilota
     public void aggiungiPilota(Pilota p) {
+        //controlla non ci sia un altro pilota con lo stesso numero di brevetto
+        for(Pilota p1 : piloti) {
+            if(p1.getNumeroBrevetto().equals(p.getNumeroBrevetto())) {
+                System.out.println("Numero di brevetto già in uso");
+                return;
+            }
+        }
         piloti.add(p);
     }
     //metodi pubblici
