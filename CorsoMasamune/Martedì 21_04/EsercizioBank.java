5089de4902797c52c6dd907c6cedca100dba3a22
Tommycik <107619877+Tommycik@users.noreply.github.com>
2026-04-22 14:51:38 +0200

    Update EsercizioBank.java

--- CorsoMasamune/Martedì 21_04/EsercizioBank.java
+++ CorsoMasamune/Martedì 21_04/EsercizioBank.java
@@ -83,7 +83,7 @@ public class EsercizioBank {
                 System.out.println("2. Withdraw");
                 System.out.println("3. Display Balance");
                 System.out.println("4. Change account");
-                System.out.println("4. Exit");
+                System.out.println("5. Exit");
                 int choice = Integer.parseInt(stringScanner.nextLine());
                 //scelta del menu
                 switch (choice) {
