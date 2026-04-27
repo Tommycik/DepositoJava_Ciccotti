d520a66dfa2646eb02f0e0e89f71eb18c047e09d
Tommycik <107619877+Tommycik@users.noreply.github.com>
2026-04-27 18:29:15 +0200

    Update GestioneScuola.java

--- CorsoMasamune/Lunedì 27_04/GestioneScuola.java
+++ CorsoMasamune/Lunedì 27_04/GestioneScuola.java
@@ -9,17 +9,21 @@ interface Registrabile{
 
 // classe astratta persona
 abstract class Persona{
+
     private int età;
     private String nome;
+
     //costruttore
     public Persona(String nome, int età){
         this.nome = nome;
         this.età = età;
     }
+
     // getter e setter
     public int getEtà(){
         return età;
     }
+
     public void setEtà(int età){
         if(età>0){
             this.età = età;
@@ -31,30 +35,39 @@ abstract class Persona{
     public String getNome(){
         return nome;
     }
+
     public void setNome(String nome){
         this.nome = nome;
     }
+
     //metodo astratto
     abstract void descriviRuolo();
+
     // stampa
     public void stampa(){
         System.out.println(nome + " " + età);
     }
+
 }
 
 // docente
 class Docente extends Persona implements Registrabile{
+
     private String materia;
+    //lista di studenti
     private ArrayList<Studente> studentiMateria = new ArrayList<Studente>();
+
     //costruttore
     public Docente(String nome, int età, String materia){
         super(nome, età);
         this.materia = materia;
     }
+
     //getter e setter
     public String getMateria(){
         return materia;
     }
+
     public void setMateria(String materia){
         this.materia = materia;
     }
@@ -62,10 +75,12 @@ class Docente extends Persona implements Registrabile{
     public ArrayList<Studente> getStudentiMateria(){
         return studentiMateria;
     }
+
     //implementazone descriviRuolo
     public void descriviRuolo(){
         System.out.println("Souno un docente di " + materia);
     }
+
     //implementazione registrazione
     public void registrazione(){
         System.out.println("Registrazione tramite segreteria didattica");
@@ -75,10 +90,12 @@ class Docente extends Persona implements Registrabile{
     public void aggiungiStudente(Studente studente){
         studentiMateria.add(studente);
     }
+
     //rimozione studente da indice
     public void rimozioneStudente(int indice){
         studentiMateria.remove(indice);
     }
+
     // assegnare voto
     public void assegnareVoto(int indice, int voto){
         if(indice < studentiMateria.size()){
@@ -87,6 +104,7 @@ class Docente extends Persona implements Registrabile{
             System.out.println("Studente non trovato");
         }
     }
+
     // recuperare voti
     public ArrayList<Integer> recuperareVoti(String materia, int indice){
         if(indice < studentiMateria.size()){
@@ -96,49 +114,62 @@ class Docente extends Persona implements Registrabile{
             return null;
         }
     }
+
 }
 
 //studente
 class Studente extends Persona{
+
     //liste multiple di voti in base alla materia
     private ArrayList<ArrayList<Integer>> votiMaterie = new ArrayList<ArrayList<Integer>>();
+    //classi frequentate
     private ArrayList<String> classiFrequentate = new ArrayList<String>();
+
     //costruttore
     public Studente(String nome, int età){
         super(nome, età);
     }
+
     //implementazione metodo astratto
     public void descriviRuolo(){
         System.out.println("Sono uno studente delle classi di " + classiFrequentate);
     }
+
     //implementazione metodo astratto
     public void registrazione(){
         System.out.println("Registrazione tramite modulo online");
     }
-    // registrare a materia
+
+    // registrare una materia
     public void registraMateria(String materia){
         if(!classiFrequentate.contains(materia.toLowerCase())){
+            //inserimento materia
             classiFrequentate.add(materia.toLowerCase());
             votiMaterie.add(new ArrayList<Integer>());
         }else{
             System.out.println("La materia " + materia + " è già stata registrata");
         }
     }
+
     // assegnare voto
     public void setVoto(int voto, String materia){
+        //controllo voto
         if(voto<0 || voto>10){
             System.out.println("Il voto non è valido");
             return;
         }
+        //Controllo posizione della materia
         if(classiFrequentate.contains(materia.toLowerCase())){
             votiMaterie.get(classiFrequentate.indexOf(materia.toLowerCase())).add(voto);
         }else{
             System.out.println("La materia " + materia + " non è registrata");
         }
     }
+
     //recuperare voti
     public ArrayList<Integer> getVoti(String materia){
         if(classiFrequentate.contains(materia.toLowerCase())){
+            //Controllo posizione della materia
             return votiMaterie.get(classiFrequentate.indexOf(materia.toLowerCase()));
         }else{
             System.out.println("La materia " + materia + " non è registrata");
@@ -148,49 +179,64 @@ class Studente extends Persona{
 }
 
 public class GestioneScuola{
+    //Funzioni helper
+    //stampa studenti
     public static void stampaStudenti(ArrayList<Studente> studenti){
         for(int i = 0; i < studenti.size(); i++){
-            System.out.println(i + ". " + studenti.get(i).getNome()
-                    + " età: " + studenti.get(i).getEtà());
+            System.out.println(i + ". Nome: " + studenti.get(i).getNome()
+                    + " Età: " + studenti.get(i).getEtà());
         }
     }
+
+    // stampa docenti
     public static void stampaDocenti(ArrayList<Docente> docenti){
         for(int i = 0; i < docenti.size(); i++){
             System.out.println(i + ". Nome: " + docenti.get(i).getNome()
-                    + " età: " + docenti.get(i).getEtà()
-                    + " materia: " + docenti.get(i).getMateria());
+                    + " Età: " + docenti.get(i).getEtà()
+                    + " Materia: " + docenti.get(i).getMateria());
         }
     }
+
+    // Scelta studente
     public static Studente scegliStudente(ArrayList<Studente> studenti, Scanner sc){
         stampaStudenti(studenti);
 
-        System.out.println("Scegli studente:");
-        int index = sc.nextInt();
-
-        if(index < 0 || index >= studenti.size()){
-            System.out.println("Indice non valido");
-            return null;
+        System.out.println("Inserisci nome studente:");
+        String nome = sc.next();
+        //ricerca
+        for(Studente s : studenti){
+            if(s.getNome().equalsIgnoreCase(nome)){
+                return s;
+            }
         }
 
-        return studenti.get(index);
+        System.out.println("Studente non trovato");
+        return null;
     }
 
+    // Scelta docente
     public static Docente scegliDocente(ArrayList<Docente> docenti, Scanner sc){
         stampaDocenti(docenti);
 
-        System.out.println("Scegli docente:");
-        int index = sc.nextInt();
-
-        if(index < 0 || index >= docenti.size()){
-            System.out.println("Indice non valido");
-            return null;
+        System.out.println("Inserisci nome docente:");
+        String nome = sc.next();
+        //ricerca
+        for(Docente d : docenti){
+            if(d.getNome().equalsIgnoreCase(nome)){
+                return d;
+            }
         }
 
-        return docenti.get(index);
+        System.out.println("Docente non trovato");
+        return null;
     }
-    public static boolean docenteEsiste(ArrayList<Docente> docenti, String materia){
+
+    //controllo esistenza docente
+    public static boolean docenteEsiste(ArrayList<Docente> docenti, String nome, String materia){
+
         for(Docente d : docenti){
-            if(d.getMateria().equalsIgnoreCase(materia)){
+            if(d.getNome().equalsIgnoreCase(nome) ||
+            d.getMateria().equalsIgnoreCase(materia)){
                 return true;
             }
         }
@@ -198,11 +244,14 @@ public class GestioneScuola{
     }
 
     public static void main(String[] args){
+        //creazione scuola
         ArrayList<Studente> studenti = new ArrayList<Studente>();
         ArrayList<Docente> docenti = new ArrayList<Docente>();
+        //creazione scanner
         Scanner intScanner = new Scanner(System.in);
         Scanner stringScanner = new Scanner(System.in);
         Scanner floatScanner = new Scanner(System.in);
+        //ciclo principale
         while (true) {
             //menu per creare docente, studente,registrare materia, assegnare voti o stampare voti
             System.out.println("1. Crea docente");
@@ -213,24 +262,21 @@ public class GestioneScuola{
             System.out.println("6. Esci");
 
             int scelta = intScanner.nextInt();
-            int studenteIndex;
-            int docenteIndex;
             Studente studente;
             Docente docente;
             ArrayList<Studente> studentiDocente;
             switch (scelta){
                 case 1:
+                    //creazione docente
                     System.out.println("Inserisci nome docente");
                     String nomeDocente = stringScanner.next();
                     System.out.println("Inserisci età docente");
                     int etàDocente = intScanner.nextInt();
-                    //controlo se gia presente docente di quella materia
-
                     System.out.println("Inserisci materia docente");
                     String materiaDocente = stringScanner.next();
-
-                    if(docenteEsiste(docenti, materiaDocente)){
-                        System.out.println("Docente già presente");
+                    //controllo se gia presente docente di quella materia
+                    if(docenteEsiste(docenti, nomeDocente, materiaDocente)){
+                        System.out.println("Docente già presente (nome o materia duplicata)");
                         break;
                     }
                     docente = new Docente(nomeDocente, etàDocente, materiaDocente);
@@ -239,10 +285,12 @@ public class GestioneScuola{
                     docente.registrazione();
                     break;
                 case 2:
+                    //creazione studente
                     System.out.println("Inserisci nome studente");
                     String nomeStudente = stringScanner.next();
                     System.out.println("Inserisci età studente");
                     int etàStudente = intScanner.nextInt();
+                    //controllo se gia presente studente di quella materia
                     boolean trovatoStudente = false;
                     for(Studente s : studenti){
                         if(s.getNome().equalsIgnoreCase(nomeStudente)){
@@ -254,57 +302,63 @@ public class GestioneScuola{
                         System.out.println("Studente gia presente");
                         break;
                     }
+                    //creazione studente
                     studente = new Studente(nomeStudente, etàStudente);
+                    //aggiunta studente
                     studenti.add(studente);
                     System.out.println("Studente creato con successo");
                     studente.registrazione();
                     break;
 
                 case 3:
+                    //registrazione studente a materia
                     System.out.println("Lista studenti");
-                    studente = scegliStudente(studenti, intScanner);
+                    //scelta studente
+                    studente = scegliStudente(studenti, stringScanner);
                     if(studente == null) break;
 
                     System.out.println("Inserisci materia");
                     String materia = stringScanner.next();
-
+                    
                     studente.registraMateria(materia);
+                    //aggiunta studente a docente di quella materia
                     for(Docente d : docenti){
                         if(d.getMateria().equalsIgnoreCase(materia)){
                             d.aggiungiStudente(studente);;
+                            break;
                         }
                     }
                     break;
 
                 case 4:
                     System.out.println("Lista docenti");
-                    //stampa lista docenti
-                    docente = scegliDocente(docenti, intScanner);
+                    //stampa lista docenti e scelta docente
+                    docente = scegliDocente(docenti, stringScanner);
                     if(docente == null){
                         System.out.println("Docente non trovato");
                         break;
                     } 
 
                     studentiDocente = docente.getStudentiMateria();
-                    if(studentiDocente == null){
+                    //controllo studenti
+                    if(studentiDocente == null || studentiDocente.isEmpty()){
                         System.out.println("Nessun studente registrato");
                         break;
                     }
                     System.out.println("Lista studenti");
-                    for(int i = 0; i < studentiDocente.size(); i++){
-                        System.out.println(i + ". Nome: " + studentiDocente.get(i).getNome() + " età: " + studentiDocente.get(i).getEtà());
-                    }
-                    System.out.println("Scegli lo studente");
-                    studenteIndex = intScanner.nextInt();
+                    //scelta studente
+                    studente = scegliStudente(studentiDocente, stringScanner);
+                    if(studente == null) break;
+                    //assegnazione voto
                     System.out.println("Scegli il voto");
                     int voto = intScanner.nextInt();
-                    docente.assegnareVoto(studenteIndex, voto);
+                    docente.assegnareVoto(studentiDocente.indexOf(studente), voto);
                     System.out.println("Voto assegnato con successo");
                     break;
                 case 5:
                     System.out.println("Lista docenti");
-                    //stampa lista docenti
-                    docente = scegliDocente(docenti, intScanner);
+                    //stampa lista docenti e scelta docente
+                    docente = scegliDocente(docenti, stringScanner);
                     if(docente == null){
                         System.out.println("Docente non trovato");
                         break;
@@ -316,18 +370,18 @@ public class GestioneScuola{
                         break;
                     }
                     System.out.println("Lista studenti");
-                    for(int i = 0; i < studentiDocente.size(); i++){
-                        System.out.println(i + ". Nome: " + studentiDocente.get(i).getNome() + " età: " + studentiDocente.get(i).getEtà());
-                    }
-                    System.out.println("Scegli lo studente");
-                    int valutatoIndex = intScanner.nextInt();
-                    ArrayList<Integer> voti = docente.recuperareVoti(docente.getMateria(), valutatoIndex);
+                    //scelta studente
+                    studente = scegliStudente(studentiDocente, stringScanner);
+                    if(studente == null) break;
+                    ArrayList<Integer> voti = docente.recuperareVoti(docente.getMateria(), studentiDocente.indexOf(studente));
+                    //stampa voti
                     System.out.println("Voti dello studente alla materia " + docente.getMateria());
                     for(int i = 0; i < voti.size(); i++){
                         System.out.println(i + ". " + voti.get(i));
                     }
                     break;
                 case 6:
+                    //Uscita dal programma
                     System.out.println("Esci dal programma");
                     return;
             }
