6f7c02c9b4a299f6351d06822dfc99455c845fe1
Tommycik <107619877+Tommycik@users.noreply.github.com>
2026-04-30 17:52:47 +0200

    Update EsercizioMedioObserver.java

--- CorsoMasamune/Giovedì 30_04/EsercizioMedioObserver.java
+++ CorsoMasamune/Giovedì 30_04/EsercizioMedioObserver.java
@@ -15,21 +15,27 @@ public class EsercizioMedioObserver {
         
         //notifica gli investitori
         agenziaBorsa.notificaInvestitori("Investimento", 1000);
+        //Scanners
         Scanner intScanner = new Scanner(System.in);
         Scanner stringScanner = new Scanner(System.in);
         Scanner doubleScanner = new Scanner(System.in);
+        //lista degli utenti
         List<Utente> utenti = new ArrayList<>();
         while(true){
+            //menu
             System.out.println("\n1. Registrazione utente");
             System.out.println("2. Login");
             System.out.println("3. Aggiorna azione");
             System.out.println("4. Investi");
-            System.out.println("5. Logout");
-            System.out.println("6. Esci");
+            System.out.println("5. Smetti di investire");
+            System.out.println("6. Logout");
+            System.out.println("7. Esci");
             int scelta = intScanner.nextInt();
             Utente utente = null;
             switch (scelta){
+                //scelta utente
                 case 1:
+                    //registrazione utente
                     System.out.println("Inserisci il tuo nome: ");
                     String nomeUtente = stringScanner.nextLine();
                     System.out.println("Inserisci la password: ");
@@ -53,12 +59,14 @@ public class EsercizioMedioObserver {
                     Sessione.getInstance().login(utente);
                     break;
                 case 2:
+                    //login utente
                     System.out.println("Nome:");
                     String nomeLogin = stringScanner.nextLine();
 
                     System.out.println("Password:");
                     String passLogin = stringScanner.nextLine();
                     Utente utenteLogin = null;
+                    //controllo nome e password
                     for (Utente u : utenti) {
                         if (u.getNome().equalsIgnoreCase(nomeLogin) && u.getPassword().equals(passLogin)) {
                             utenteLogin = u;
@@ -73,6 +81,8 @@ public class EsercizioMedioObserver {
                     }
                     break;
                 case 3:
+                    //aggiorna azione
+                    //controllo sessione
                     if(Sessione.getInstance().getUtente() == null){
                         System.out.println("Non hai effettuato il login");
                         break;
@@ -84,6 +94,7 @@ public class EsercizioMedioObserver {
                     agenziaBorsa.aggiornaValoreAzione(azione, valore);
                     break;
                 case 4:
+                    //investi
                     if(Sessione.getInstance().getUtente() == null){
                         System.out.println("Non hai effettuato il login");
                         break;
@@ -92,6 +103,16 @@ public class EsercizioMedioObserver {
                     System.out.println("Hai investito!");
                     break;
                 case 5:
+                    //smetti di investire
+                    if(Sessione.getInstance().getUtente() == null){
+                        System.out.println("Non hai effettuato il login"); 
+                        break;
+                    }
+                    agenziaBorsa.removeInvestitore(Sessione.getInstance().getUtente());
+                    System.out.println("Hai smesso di investire");
+                    break;
+                case 6:
+                    //logout
                     if(Sessione.getInstance().getUtente() == null){
                         System.out.println("Non hai effettuato il login");
                         break;
@@ -100,7 +121,8 @@ public class EsercizioMedioObserver {
                     }
                     break;
 
-                case 6:
+                case 7:
+                    //esci
                     System.out.println("Arrivederci!");
                     return;
                 default:
@@ -141,45 +163,50 @@ class AgenziaBorsa{
     }
 }
 
+//utente privato
 class UtentePrivato extends Utente {
-
+    //costruttore
     public UtentePrivato(String nome, String password) {
         super(nome, password);
     }
-
+    // override notifica
     @Override
     public void notifica(String azione, double valore) {
         System.out.println(nome + " (Privato): azione " + azione + " = " + valore);
     }
 }
+//classe astratta utente
 abstract class Utente implements Investitore {
     protected String nome;
     protected String password;
-
+    //costruttore
     public Utente(String nome, String password) {
         this.nome = nome;
         this.password = password;
     }
-
+    //getter e setter
     public String getNome() {
         return nome;
     }
-
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
     public String getPassword() {
         return password;
     }
+    //override notifica
      @Override
     public void notifica(String azione, double valore) {
         System.out.println(nome + " ha ricevuto aggiornamento: " + azione + " = " + valore);
     }
 }
-
+//utente bancario
 class UtenteBancario extends Utente{
-
+    //costruttore
     public UtenteBancario(String nome, String password) {
         super(nome, password);
     }
-
+    //override notifica
     @Override
     public void notifica(String azione, double valore) {
         System.out.println(nome + " (Banca) avvisa cliente -> " + azione + " = " + valore);
@@ -188,18 +215,20 @@ class UtenteBancario extends Utente{
 
 //Singleton sessione
 class Sessione {
+    //instanza
     private static Sessione instance;
+    //utente loggato
     private Utente utenteLoggato;
-
+    //costruttore privato
     private Sessione() {}
-
+    //metodo per ottenere istanza
     public static Sessione getInstance() {
         if (instance == null) {
             instance = new Sessione();
         }
         return instance;
     }
-
+    //metodo per effettuare il login
     public void login(Utente u) {
         if(u == null){
             System.out.println("Utente non esistente");
@@ -207,10 +236,11 @@ class Sessione {
         }
         this.utenteLoggato = u;
     }
-
+    //metodo per ottenere l'utente loggato
     public Utente getUtente() {
         return utenteLoggato;
     }
+    //metodo per effettuare il logout
     public void logout() {
         if (utenteLoggato != null) {
             System.out.println("Logout di " + utenteLoggato.getNome());
